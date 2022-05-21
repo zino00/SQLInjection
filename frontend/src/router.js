ac318b1d1740a49f0c0bf67a80d9bc62c0ae99bf
Kiprey <Kiprey@qq.com>
2022-05-21 14:48:17 +0800

    add all other page

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -35,7 +35,31 @@ const constantRoutes = [
         path: 'bool',
         component: () => import('@/views/sql/bool'),
         name: 'BoolTest',
-        meta: { title: '布尔注入', noCache: true }
+        meta: { title: '布尔盲注', noCache: true }
+      },
+      {
+        path: 'time',
+        component: () => import('@/views/sql/time'),
+        name: 'TimeTest',
+        meta: { title: '时间盲注', noCache: true }
+      },
+      {
+        path: 'union',
+        component: () => import('@/views/sql/union'),
+        name: 'UnionTest',
+        meta: { title: '联合注入', noCache: true }
+      },
+      {
+        path: 'error',
+        component: () => import('@/views/sql/error'),
+        name: 'ErrorTest',
+        meta: { title: '报错注入', noCache: true }
+      },
+      {
+        path: 'wide_char',
+        component: () => import('@/views/sql/wide_char'),
+        name: 'WideCharTest',
+        meta: { title: '宽字节注入', noCache: true }
       }
     ]
   }
